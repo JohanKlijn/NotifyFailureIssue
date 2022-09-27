ee45e0524643446a214589ad1ad8329122a39bb4
Johan Klijn <johan.klijn@cybertwice.com>
2022-09-27 11:03:09 +0200

    Implemented example to display notifyFailure issue.

--- NotifyFailureIssue/tabs/src/components/Tab.tsx
+++ NotifyFailureIssue/tabs/src/components/Tab.tsx
@@ -1,14 +1,37 @@
-import { useContext } from "react";
-import { Welcome } from "./sample/Welcome";
-import { TeamsFxContext } from "./Context";
-
-const showFunction = Boolean(process.env.REACT_APP_FUNC_NAME);
+import { useEffect, useState } from "react";
+import { app } from "@microsoft/teams-js";
 
 export default function Tab() {
-  const { themeString } = useContext(TeamsFxContext);
+  const [error, setError] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+
+    const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    const initializeApp = async () => {
+      app.initialize()
+      .then(() => {
+        setLoading(false)
+      })
+      .catch((reason) =>{
+        setError(reason)
+      })
+    }    
+    
+    wait(3 * 1000).then(() => {
+      initializeApp()
+      .then(() => {
+        console.log({"isInitialized" : app.isInitialized()})
+        app.notifyFailure({reason: app.FailedReason.Other, message: "My custom failure message."}); 
+      })     
+    })
+
+  }, []);
+
   return (
-    <div className={themeString === "default" ? "" : "dark"}>
-      <Welcome showFunction={showFunction} />
+    <div >
+      My Teams Tab App
     </div>
   );
 }
